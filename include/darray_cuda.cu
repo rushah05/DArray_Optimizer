979abfbdd65c1022dd1285a2fcfff380e1960fe5
rushah05 <46826537+rushah05@users.noreply.github.com>
2022-07-26 14:48:24 -0500

    RBF Kernel matrix multiplication done

--- include/darray_cuda.cu
+++ include/darray_cuda.cu
@@ -3,14 +3,16 @@
 #include <cublas_v2.h>
 #include <cuda_fp16.h>
 #include <cuda_runtime.h>
+#include <cusolverDn.h>
 #include <stdio.h>
 
 cublasHandle_t handle;
-cublasStatus_t stat;
+cusolverDnHandle_t csHandle;
 cudaStream_t stream;
-	
-float *mem;
-__half *half_mem;
+cudaError_t cudaStat;
+cublasStatus_t stat;
+cusolverStatus_t statusH = CUSOLVER_STATUS_SUCCESS;
+
 
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
@@ -24,16 +26,15 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 
 void cuda_init(){
     cublasCreate(&handle);
+    cusolverDnCreate(&csHandle);
     cudaStreamCreate(&stream);
     cublasSetStream(handle, stream);
-    // cudaMalloc(&mem, sizeof(float)*65536*65536);
-	// cudaMalloc(&half_mem, sizeof(__half)*65536*65536);
+    cusolverDnSetStream(csHandle, stream);
 }
 
 void cuda_finalize(){
-    // cudaFree(mem);
-	// cudaFree(half_mem);
 }
+
 
 template<typename T>
 void printMatrixDeviceBlock(char *filename,int m, int n, T* dA, int lda)
@@ -51,7 +52,7 @@ void printMatrixDeviceBlock(char *filename,int m, int n, T* dA, int lda)
             cudaMemcpy(&ha[0], &dA[i+j*lda], sizeof(float), cudaMemcpyDeviceToHost);
             fprintf(f, "%lf", ha[0]);
             if (j == n - 1) fprintf(f, "\n");
-			else fprintf(f, ",");
+			else fprintf(f, " ,");
         }
     }
     fclose(f);
@@ -83,143 +84,150 @@ void printVectorDeviceBlock(char *filename, int m, T* dA)
 }
 
 
-// CUDA kernel to generate the matrix, block by block.
-// the result will be store in buf, column major, m*n matrix,
-// with LDA m.
-// XIJ/buf are column major.
-// could be improved by assigning more work to each thread.
 template<typename FloatType, typename GammaType>
 __global__
-void rbf( int m, int n, FloatType *buf, int ldb, FloatType *Xi, FloatType *Xj, FloatType *Xij, 
-int ldxij, GammaType gamma, FloatType *Yi, FloatType *Yj)
+void rbf( int m, int n, FloatType *buf, int ldb, FloatType *Xi, FloatType *Xj, FloatType *Xij, int ldxij, GammaType gamma, FloatType *Yi, FloatType *Yj)
 {
 	int i=blockIdx.x*blockDim.x + threadIdx.x;
 	int j=blockIdx.y*blockDim.y + threadIdx.y;
 
 	if (i<m && j<n) {
 		buf[i+j*ldb] = Yi[i]*Yj[j]*__expf(-gamma*(Xi[i] + Xj[j] - 2*Xij[i+j*ldxij]));
+		// printf("[i,j]=[%d,%d], buf[]=%.4f, XI[]=%.4f, XJ[]=%.4f, XIJ[]=%.4f\n", i, j, buf[i+j*ldb],
+		// 	   XI[i], XJ[j], XIJ[i+j*ldxij]);
 	}
 }
 
+
 template<typename FloatType>
-__global__ void vecnorm(FloatType *Z, int ldz, FloatType *Zi, int m, int k){
-    int i=blockIdx.x*blockDim.x+threadIdx.x;
-    if(i < m){
-        float sum=0.0f;
+__global__ 
+void vecnorm(FloatType *Zd, int ldz, FloatType *ZI, int m, int k){
+	int i=blockIdx.x*blockDim.x + threadIdx.x;
+	if( i<m ) {
+		FloatType sum = 0.0f;
+
         #pragma unroll (4)
-        for( int j=0; j<k; j++){
-            sum+=Z[i+j*ldz]*Z[i+j*ldz];
-        }
-        Zi[i]=sum;
+		for( int j=0; j<k; j++ )
+			sum += Zd[i+j*ldz]*Zd[i+j*ldz];
+
+		ZI[i] = sum;
+	}
+}
+
+__global__ 
+void fnorm( int m, int n, double *buf, int B, double *XI, double *XJ, double *XIJ, int ldxij, double gamma, double *YI, double *YJ, double *acc){
+	int i=blockIdx.x*blockDim.x + threadIdx.x;
+	int j=blockIdx.y*blockDim.y + threadIdx.y;
+
+	if (i<m && j<n) 
+		// buf[i+j*ldb] = YI[i]*YJ[j]*__expf(-gamma*(XI[i] + XJ[j] - 2*XIJ[i+j*ldxij]));
+		acc += 0;
+}
+
+
+__global__ void s2h(float *Z, __half *hZ, int ldz, int m, int n){
+    int i=blockIdx.x*blockDim.x+threadIdx.x;
+    int j=blockIdx.y*blockDim.y+threadIdx.y;
+
+    if(i < m && j < n){
+        hZ[i+j*ldz] = __float2half(Z[i+j*ldz]);
     }
 }
 
-__global__ void vecnormT(float *Z, int ldz, float *Zi, int k, int m){
+__global__ void h2s(__half *hZ, float *Z, int ldz, int m, int n){
     int i=blockIdx.x*blockDim.x+threadIdx.x;
-    if(i < m){
-        float sum=0.0f;
-        #pragma unroll (4)
-        for( int j=0; j<k; j++){
-            sum+=Z[j+i*ldz]*Z[j+i*ldz];
-        }
-        Zi[i]=sum;
+    int j=blockIdx.y*blockDim.y+threadIdx.y;
+
+    if(i < m && j < n){
+        Z[i+j*ldz] = __half2float(hZ[i+j*ldz]);
     }
 }
 
 
-void lra(int rank, int n, int d, int k, float* Xi, int ldxi, float* Xj, int ldxj, float* Yi, float* Yj, float* KOmega, int ldk, float gamma, float* Omega, int ldo){
-    const int B = 8192;
-    float *dk, *dXij, *dXi, *dYi, *dXj, *dYj, *dXi_sqr, *dXj_sqr, *dO, *dKO;
-    gpuErrchk(cudaMalloc(&dk, sizeof(float)*B*B));
-    gpuErrchk(cudaMalloc(&dXij, sizeof(float)*B*B));
-    gpuErrchk(cudaMalloc(&dO, sizeof(float)*B*k));
-    gpuErrchk(cudaMalloc(&dKO, sizeof(float)*B*k));
+__global__ void s2h_vec(float *Z, __half *hZ, int m){
+    int i=blockIdx.x*blockDim.x+threadIdx.x;
+
+    if(i < m){
+        hZ[i] = __float2half(Z[i]);
+    }
+}
+
+
+
+
+void lra(int rank, int gn, int ln, int d, int k, float* Xi, int ldxi, float* Xj, int ldxj, float* Yi, float* Yj, float* KO, int ldk, float gamma, float* Omega, int ldo){
+    const int B=16384;
+    if(rank ==0) printf("gn::%d, ln::%d, d::%d, k::%d\nldxi::%d, ldxj::%d, ldk::%d, ldo::%d\n", gn, ln, d, k, ldxi, ldxj, ldk, ldo);
+    float *dXi, *dXj, *dYi, *dYj, *dO, *dKO;
+    gpuErrchk(cudaMalloc(&dXi, sizeof(float)*ln*d));
+    gpuErrchk(cudaMalloc(&dXj, sizeof(float)*gn*d));
+    gpuErrchk(cudaMalloc(&dYi, sizeof(float)*ln));
+    gpuErrchk(cudaMalloc(&dYj, sizeof(float)*gn));
+    gpuErrchk(cudaMalloc(&dO, sizeof(float)*gn*k));
+    gpuErrchk(cudaMalloc(&dKO, sizeof(float)*ln*k));
+    gpuErrchk(cudaMemcpy(dXi, Xi, sizeof(float)*ln*d, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(dXj, Xj, sizeof(float)*gn*d, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(dYi, Yi, sizeof(float)*ln, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(dYj, Yj, sizeof(float)*gn, cudaMemcpyHostToDevice));
+    gpuErrchk(cudaMemcpy(dO, Omega, sizeof(float)*gn*k, cudaMemcpyHostToDevice));
+
+    float *dXi_sqr, *dXj_sqr, *dk, *dXij;
     gpuErrchk(cudaMalloc(&dXi_sqr, sizeof(float)*B));
     gpuErrchk(cudaMalloc(&dXj_sqr, sizeof(float)*B));
-    gpuErrchk(cudaMalloc(&dXi, sizeof(float)*B*d));
-    gpuErrchk(cudaMalloc(&dXi, sizeof(float)*B*d));
-    gpuErrchk(cudaMalloc(&dXj, sizeof(float)*d*B));
-    gpuErrchk(cudaMalloc(&dYi, sizeof(float)*B));
-    gpuErrchk(cudaMalloc(&dYj, sizeof(float)*B));
-    cudaPeekAtLastError();
+    gpuErrchk(cudaMalloc(&dk, sizeof(float)*B*B));
+    gpuErrchk(cudaMalloc(&dXij, sizeof(float)*B*B));
     cudaDeviceSynchronize();
-    // printf("rank::%d, n::%d, d::%d, k::%d\n", rank, n, d, k);
-    // char r=rank;
-    // char name[] = { 'X','i','_',r,'.','t','x','t','\0' }; 
-    // printMatrixDeviceBlock(name, n, d, dXi, ldxi);
-    // name[1]='j';
-    // printMatrixDeviceBlock(name, d, n, dXj, ldxj);
 
     float sone=1.0f;
-	float szero=0.0f;
-    for(int i=0; i<n; i+=B){
-        int ib=min(B, n-i);
-        for(int j=0; j<n; j+=B){
-            int jb=min(B, n-i);
-            gpuErrchk(cudaMemcpy(dXi, &Xi[i], sizeof(float)*ib*d, cudaMemcpyHostToDevice));
-            gpuErrchk(cudaMemcpy(dXj, &Xj[j], sizeof(float)*d*jb, cudaMemcpyHostToDevice));
-            gpuErrchk(cudaMemcpy(dYi, &Yi[i], sizeof(float)*ib, cudaMemcpyHostToDevice));
-            gpuErrchk(cudaMemcpy(dYj, &Yj[j], sizeof(float)*jb, cudaMemcpyHostToDevice));
-            cudaPeekAtLastError();
-            // if(rank == 0) printf("rank::%d, (B+63)/64::%d, ldxi::%d, ib::%d, d::%d\n", rank, (B+63)/64, ldxi, ib, d);
-            vecnorm<<<(B+63)/64, 64>>>(dXi, ib, dXi_sqr, ib, d);
-            vecnormT<<<(B+63)/64, 64>>>(dXj, d, dXj_sqr, d, jb);
-            // // char name[] = { 'X','i','_',r,'.','t','x','t','\0' }; 
-            // // printVectorDeviceBlock(name, ib, dXi_sqr);
-            // // name[1]='j';
-            // // printVectorDeviceBlock(name, jb, dXj_sqr);
-            if(d >= 128){
-                stat=cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_N, ib, jb, d, &sone, dXi, CUDA_R_32F, ib, dXj, CUDA_R_32F, d, &szero, dXij,
-                    CUDA_R_32F, ib, CUDA_R_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
-            }else{    
-                stat=cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, ib, jb, d, &sone, dXi, ib, dXj, d, &szero, dXij, ib);
-            }
-            if (stat != CUBLAS_STATUS_SUCCESS) {
-                printf ("cublasSgemm failed %s\n", __LINE__);
-            }
-            // char name[] = { 'X','i','j','_', r,'.','t','x','t','\0' }; 
-            // printMatrixDeviceBlock(name, ib, jb, dXij, ib);
+    float szero=0.0f;
+    for (int i=0; i<ln; i+=B) {
+		int ib = min(B, ln-i);
+		for (int j=0; j<gn; j+=B) {
+			int jb = min(B, gn-j);
 
-            dim3 threadsPerBlock(32,32);
-			dim3 numBlocks( (ib+threadsPerBlock.x-1)/threadsPerBlock.x,
+			// printf("[i=%d j=%d] ib::%d, jb::%d\n", i,j, ib, jb);
+			// step 1: populate XI, XJ, XIJ
+			vecnorm<<<(B+63)/64, 64>>>(&dXi[i], ldxi, dXi_sqr, ib, d);
+            gpuErrchk( cudaPeekAtLastError() );
+			vecnorm<<<(B+63)/64, 64>>>(&dXj[j], ldxj, dXj_sqr, jb, d);
+			gpuErrchk( cudaPeekAtLastError() );
+
+			// XIJ is column major!!
+			// printf("ib=%d jb=%d d=%d ldz=%d\n", ib, jb, d, ldz);
+			stat = cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_T, ib, jb, d,
+								   &sone, &dXi[i], CUDA_R_32F,ldxi,
+								   &dXj[j], CUDA_R_32F,ldxj, &szero,
+								   dXij, CUDA_R_32F, ib,
+								   CUDA_R_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);	
+			if (stat != CUBLAS_STATUS_SUCCESS) printf ("cublasGemmEx failed %s\n", __LINE__);
+			gpuErrchk( cudaPeekAtLastError() );
+
+			dim3 threadsPerBlock(32,32);
+			dim3 numBlocks((ib+threadsPerBlock.x-1)/threadsPerBlock.x,
 							(jb+threadsPerBlock.y-1)/threadsPerBlock.y );
 
-			rbf<<<numBlocks, threadsPerBlock>>>( ib, jb, dk, B, dXi, dXj, dXij, ib, gamma, &dYi[i], &dYj[j]);
-            gpuErrchk(cudaPeekAtLastError());
-            cudaDeviceSynchronize();
+			// printf("ib=%d, jb=%d, B=%d, TPB.(x,y)=(%d,%d), B.(x,y)=(%d,%d)\n",
+			// 	   ib, jb, B, threadsPerBlock.x, threadsPerBlock.y,
+			// 	   numBlocks.x, numBlocks.y);
+			rbf<<<numBlocks, threadsPerBlock>>>( ib, jb, dk, ib, dXi_sqr, dXj_sqr, dXij, ib, gamma, &dYi[i], &dYj[j]);
+			gpuErrchk( cudaPeekAtLastError());
+			gpuErrchk( cudaDeviceSynchronize());
 
-            gpuErrchk(cudaMemcpy(dO, &Omega[j], sizeof(float)*jb*k, cudaMemcpyHostToDevice));
-            if (k>1) {
-				if (k < 256) {
-					cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_N, ib, k, jb,
-								&sone, dk, ib, dO, jb,
-								&sone, dKO, ib);
-				}
-				else {
-					cublasGemmEx( handle, CUBLAS_OP_N, CUBLAS_OP_N, ib, k, jb, 
-								  &sone, dk, CUDA_R_32F, ib, dO, CUDA_R_32F, jb,
-					 			  &sone, dKO, CUDA_R_32F, ib, 
-					 			  CUDA_R_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
-				}
-			} else if (k==1) {
-				cublasSgemv(handle, CUBLAS_OP_N, ib, jb, &sone, dk, ib, dO, 1, &sone, dKO, 1);
-			}
-
-			gpuErrchk( cudaPeekAtLastError() );
-			cudaDeviceSynchronize();
-
-            // gpuErrchk(cudaMemcpy(&KOmega[j], dKO, sizeof(float)*ib*d, cudaMemcpyDeviceToHost));
-
+            stat = cublasGemmEx( handle, CUBLAS_OP_N, CUBLAS_OP_N, ib, k, jb, 
+                                &sone, dk, CUDA_R_32F, B, &dO[j], CUDA_R_32F, ldo,
+                                &sone, &dKO[i], CUDA_R_32F, ldk, 
+                                CUDA_R_32F, CUBLAS_GEMM_DEFAULT_TENSOR_OP);
+            if (stat != CUBLAS_STATUS_SUCCESS) printf ("cublasGemmEx failed %s\n", __LINE__);
         }
     }
 
-    cudaFree(dk);
-    cudaFree(dXij);
-	cudaFree(dXi);
-    cudaFree(dXj);
-    cudaFree(dYi);
-    cudaFree(dYj);
-    cudaFree(dXi_sqr);
-    cudaFree(dXj_sqr);
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaMemcpy(KO, dKO, sizeof(float)*ln*k, cudaMemcpyDeviceToHost));
+    cudaPeekAtLastError();
 }
 
+
+
+void SGEQRF(int *m, int *n, float *Q, int *ldq, float *tau, float *work, int *lwork, int *info){
+
+}
--- tests/testsvm.cpp
+++ tests/testsvm.cpp
@@ -1,10 +1,27 @@
 #include "darray.h"
-#include "darray_convex.h"
 #include "read_input.h"
+#include "lapack_like.h"
 
 extern void cuda_init();
 extern void cuda_finalize();
-extern void lra(int rank, int n, int d, int k, float* Xi, int ldxi, float* Xj, int ldxj, float* Yi, float* Yj, float* KOmega, int ldk, float gamma, float* Omega, int ldo);
+// extern void lra(int rank, int n, int d, int k, float* Xi, int ldxi, float* Xj, int ldxj, float* Yi, float* Yj, float* K, int ldk, float gamma);
+extern void lra(int rank, int gn, int ln, int d, int k, float* Xi, int ldxi, float* Xj, int ldxj, float* Yi, float* Yj, float* KOmega, int ldk, float gamma, float* Omega, int ldo);
+extern void SGEQRF(int *m, int *n, float *Q, int *ldq, float *tau, float *work, int *lwork, int *info);
+
+template<typename T>
+DArray::LMatrix<T> QR(DArray::DMatrix<T> A, int i1, int j1, int i2, int j2) {
+  // 1. replicate in rows: rows distributed, col replicated
+  auto LA = A.replicate_in_rows(i1,j1,i2,j2);
+  int m = LA.dims()[0], n = LA.dims()[1];
+  assert( n == j2 - j1);
+  assert( m > n);
+
+  // 2. local QR (BLAS)
+  std::vector<T> tau(std::min(m,n));
+  int lwork = -1, info ;
+  T tmp;
+  SGEQRF( &m, &n, LA.data(), &LA.ld(), tau.data(), &tmp, &lwork, &info);
+}
 
 int main(int argc, char** argv){
   MPI_Init(&argc, &argv);
@@ -13,52 +30,92 @@ int main(int argc, char** argv){
   DArray::Grid::np_to_pq(np, p, q); // np = p*q
   DArray::Grid g(MPI_COMM_WORLD, p, q);
   g.barrier();
-
   cuda_init();
-  long long int gn=5000000;
-	int d=18;
-	int k=256;
+  DArray::ElapsedTimer timer;
+
+  long long int gn=49980;
+	int d=22;
+	int k=64;
 	int qq=1;
 	float gamma=0.0001f;
-	char *filename="/project/pwu/dataset/SUSY";
-  if(g.rank()==0) printf("#### SVM Kernel Training (Grid::%dx%d, gn::%d, d::%d, k::%d)\n", p, q, gn, d, k);
+	char *filename="/project/pwu/dataset/ijcnn1";
+
+  if(g.rank()==0) printf("#### SVM Kernel Training (Grid::%dx%d, gn::%d, d::%d, k::%d) \n", p, q, gn, d, k);
   float *x=(float*)malloc(sizeof(float)*d*gn);
   float *y=(float*)malloc(sizeof(float)*gn);
+  for(int i=0; i<d; i++){
+    for(int j=0; j<gn; j++){
+      x[i+j*d]=0.0;
+    }
+  }
+  for(int i=0; i<gn; ++i){
+    y[i]=0.0;
+  }
   read_input_file(filename, gn, d, k, x, d, y);
   if(g.rank()==0) printf("Finished Reading input \n");
   g.barrier();
 
   DArray::DMatrix<float> X(g, d, gn);
   DArray::DMatrix<float> Y(g, 1, gn);
-  DArray::LMatrix<float> Omega(X.ldims()[0], k);
-  if(g.rank()==0) printf("X[%d,%d], Xlocal[%d,%d] \n", X.dims()[0], X.dims()[1], X.ldims()[0], X.ldims()[1]);
+  DArray::DMatrix<float> Omega(g, gn, k);
   X.set_value_from_matrix(x, d);
-  Y.set_value_from_vector(y);
+  Y.set_value_from_matrix(y, 1);
   Omega.set_normal_seed(0, 1, 1);
   // X.collect_and_print("X");
   // Y.collect_and_print("Y");
   // Omega.collect_and_print("Omega");
-  g.barrier();
-  if(g.rank()==0) printf("Elemental Distribution Done \n");
+  // if(g.rank()==0) printf("Elemental Distribution Done \n");
+  // g.barrier();
 
-  // printf("rank::%d, X[%d,%d], Y[%d,%d], Omega[%d,%d]\n", g.rank(), X.dims()[0], X.dims()[1], Y.dims()[0], Y.dims()[1], Omega.dims()[0], Omega.dims()[1]);
-  DArray::LMatrix<float> Xi = X.transpose_and_replicate_in_rows(0, 0, X.dims()[0], X.dims()[1]);
-  DArray::LMatrix<float> Xj = X.replicate_in_columns(0, 0, X.dims()[0], X.dims()[1]);
-  DArray::LMatrix<float> Yi = Y.transpose_and_replicate_in_rows(0, 0, Y.dims()[0], Y.dims()[1]);
-  DArray::LMatrix<float> Yj = Y.replicate_in_columns(0, 0, Y.dims()[0], Y.dims()[1]);
-  
-  if(g.rank()==0) {
-    printf("Xi,Xj,Yi, and Yj  row and column replicated \n");
-    // printf("X[%d,%d], Xi[%d,%d], Xj[%d,%d]\n", X.dims()[0], X.dims()[1], Xi.dims()[0], Xi.dims()[1], Xj.dims()[0], Xj.dims()[1]);
+  // Distributed Low-Rank Approximated Kernel
+  DArray::DMatrix<float> KO(g, gn, k);
+  KO.set_zeros();
+  {
+    DArray::LMatrix<float> Xi = X.transpose_and_replicate_in_rows(0, 0, d, gn);
+    DArray::LMatrix<float> Xj = X.replicate_in_all(0, 0, d, gn);
+    DArray::LMatrix<float> Yi = Y.transpose_and_replicate_in_rows(0, 0, 1, gn);
+    DArray::LMatrix<float> Yj = Y.replicate_in_all(0, 0, 1, gn);
+    DArray::LMatrix<float> XjT = Xj.transpose();
+    DArray::LMatrix<float> YjT = Yj.transpose();
+
+    g.barrier();
+    // if(g.rank()==0) Xi.print("Xi");
+    // if(g.rank()==0) Xj.print("Xj");
+    // if(g.rank()==0) Yi.print("Yi");
+    // if(g.rank()==0) Yj.print("Yj");
+    
+
+    DArray::LMatrix<float> buff = KO.replicate_in_rows(0, 0, gn, k);
+    DArray::LMatrix<float> Omegal = Omega.replicate_in_columns(0, 0, gn, k);
+    buff.set_zeros();
+    // if(g.rank()==0) Omegal.print("Omegal");
+    // if(g.rank()==0) buff.print("buff");
+    if(g.rank()==0) printf("Xi[%d,%d], Xj[%d,%d], XjT[%d,%d], \nOmega_l[%d,%d] \nbuff[%d,%d] \n", Xi.dims()[0], Xi.dims()[1], Xj.dims()[0], Xj.dims()[1],
+    XjT.dims()[0], XjT.dims()[1], Omegal.dims()[0], Omegal.dims()[1], buff.dims()[0], buff.dims()[1]);
+
+    int ln = Xi.dims()[0];
+    int gd = Xi.dims()[1];
+    int lk = Omegal.dims()[1];
+    timer.start();
+    lra(g.rank(), gn, ln, gd, lk, Xi.data(), Xi.dims()[0], XjT.data(), XjT.dims()[0], Yi.data(), YjT.data(), buff.data(), buff.dims()[0], gamma, Omegal.data(), Omegal.dims()[0]);
+    int ms = timer.elapsed();
+    if(g.rank()==0) fmt::print("P[{},{}]: LRA takes: {}(ms)\n",  g.ranks()[0], g.ranks()[1], ms);
+    KO.dereplicate_in_all(buff, 0, 0, buff.dims()[0], buff.dims()[1]);
   }
-  
-  g.barrier();
 
-  DArray::LMatrix<float> buff(Xi.dims()[1], k);
-  // X is d*n
-  lra(g.rank(), X.ldims()[1], X.ldims()[0], Omega.dims()[1], Xi.data(), Xi.dims()[0], Xj.data(), Xj.dims()[0], Yi.data(), Yj.data(), buff.data(), buff.dims()[0], gamma, Omega.data(), Omega.dims()[0]);
-  if(g.rank()==0) printf("LRA done \n");
+  // KO.collect_and_print("KO");
+  auto fnorm = KO.fnorm();
+  if(g.rank()==0) fmt::print("norm(K)={}\n", fnorm);
+
+  // Distributed QR
+  DArray::DMatrix<float> Q = KO.clone();
+  DArray::LMatrix<float> R = QR(Q, 0, 0, gn, k);
+  if(g.rank()==0) printf("Finished QR\n");
+
 
   cuda_finalize();
   MPI_Finalize();
 }
+
+
+
