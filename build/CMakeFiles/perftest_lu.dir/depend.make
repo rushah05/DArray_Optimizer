61758f7dcf59fc67e1eb14086234dc847b1412c3
rushah05 <46826537+rushah05@users.noreply.github.com>
2022-10-14 21:30:07 -0500

    Delete build directory

--- build/CMakeFiles/perftest_lu.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for perftest_lu.
-# This may be replaced when dependencies are built.
--- build/perfchol.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-  
-#SBATCH -N 4
-#SBATCH -n 64
-#SBATCH --distribution=block:block
-
-
-module load GCC intel
-MKL_NUM_THREADS=1 OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 mpirun ./perftest_cholesky -n 50000 
